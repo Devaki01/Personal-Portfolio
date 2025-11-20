abce60f2d06db57552862aaae1d79f387314db3e
Devaki01 <devakijoshi.2005@gmail.com>
2025-11-20 15:37:06 +0530

    navbar flex

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,10 +1,14 @@
 import '../App.css';
 import Navbar from './navbar';
+import logo from '../assets/logo.png';
 
 const Home = () => {
   return (
     <>
-      <Navbar />
+      <div className='flex items-center justify-between bg-black'> 
+        <div className='p-4'> <img src={logo} /> </div>
+        <Navbar />
+      </div>
       <h1> Home Page</h1>
     </>
   );
