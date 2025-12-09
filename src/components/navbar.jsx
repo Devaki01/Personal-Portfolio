b9ec4431aa555125e9d8b2da291890b229663a2b
Devaki01 <devakijoshi.2005@gmail.com>
2025-12-10 04:36:26 +0530

    creating hamburger menu

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,31 +1,68 @@
-import '../App.css';
-import { Link } from 'react-router-dom';
+import {useState} from 'react';
+import {Link} from 'react-router-dom';
+import Hamburger from 'hamburger-react';
 
 const Navbar = () => {
+  const [open, setOpen] = useState(false);
+  const handleLinkClick = () => setOpen(false);
+
   return (
-    <nav>
+    <nav className="relative w-full z-50">
       
-      <div className='font-spartan text-white text-3xl p-2 bg-black flex-column'>
+      <div className="fixed top-5 right-5 z-50">
+        <Hamburger 
+          toggled={open} 
+          toggle={setOpen} 
+          size={32}
+          color={open ? '#4F46E5' : '#FFFFFF'} 
+          duration={0.4}
+        />
+      </div>
 
-        <Link to="/" className="p-4 hover:text-primary">
-          Home
-        </Link>
+      <div 
+        className={`fixed top-0 right-0 h-full w-full md:w-1/2 bg-indigo-100 shadow-xl transform transition-transform duration-300 ease-in-out ${
+          open ? 'translate-x-0' : 'translate-x-full'
+        } flex flex-col justify-center items-center`}
+      >
 
-        <Link to="/about" className="p-4 hover:text-primary">
-          About
-        </Link>
+        <div className="flex flex-col space-y-12 text-center font-bold text-3xl md:text-6xl text-indigo-600 font-spartan">
+          
+          <Link 
+            to="/" 
+            onClick={handleLinkClick}
+            className="hover:text-indigo-800 transition-colors duration-200"
+          >
+            Home
+          </Link>
 
-        <Link to="/projects" className="p-4 hover:text-primary">
-          Projects
-        </Link>
+          <Link 
+            to="/about" 
+            onClick={handleLinkClick}
+            className="hover:text-indigo-800 transition-colors duration-200"
+          >
+            About Me
+          </Link>
 
-        <Link to="/contact" className="p-4 hover:text-primary">
-          Contact
-        </Link>
-        
+          <Link 
+            to="/projects" 
+            onClick={handleLinkClick}
+            className="hover:text-indigo-800 transition-colors duration-200"
+          >
+            Projects
+          </Link>
+
+          <Link 
+            to="/contact" 
+            onClick={handleLinkClick}
+            className="hover:text-indigo-800 transition-colors duration-200"
+          >
+            Contact
+          </Link>
+
+        </div>
       </div>
     </nav>
   );
 };
 
-export default Navbar
+export default Navbar;
